8f56342dfb27ff4d5d22c7e4070334834529fc8d
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2022-02-18 19:09:39 +0530

    Third Commit: TestCase updated as per CR

--- OHRMCode/src/testcases/TestCase2.java
+++ OHRMCode/src/testcases/TestCase2.java
@@ -5,6 +5,7 @@ public class TestCase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is TestCase2");
+		System.out.println("TestCase2 Updated...");
 
 	}
 
